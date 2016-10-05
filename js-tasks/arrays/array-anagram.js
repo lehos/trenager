feee03aac61fd461e6d1f1c4d7bb839be283dba6
Alexey Stratan <alexey.stratan@gmail.com>
2016-10-05 22:41:05 +0300

    array anagram

---
+++ js-tasks/arrays/array-anagram.js
@@ -0,0 +1,34 @@
+// Напишите функцию, принимающую массив произвольных слов и на выходе дающую двумерный массив анаграмм:
+// ['стол', 'барокко', слот', 'кот', 'кошка', 'ток', 'коробка']
+// ->
+// [['стол', 'слот'], ['кот', 'ток'], ['барокко', 'коробка']]
+
+function arrayAnagram(arr) {
+    let result = []
+    let sorted = arr.map((elem) => elem.split('').sort().join(''))
+    //storage for second anagram indexes, to exclude them in next iterations
+    let trash = []
+
+    arr.forEach((elem, i) => {
+        // if we're iterating over second anagram - quit
+        if (trash.indexOf(i) != -1) {
+            return
+        }
+
+        // get index of second anagram
+        let j = sorted.indexOf(sorted[i], i + 1)
+
+        // if there is no anagram - quit
+        if (j == -1) {
+            return
+        }
+
+        // remember used anagrams to exclude them in next iterations
+        trash.push(j)
+
+        result.push([elem, arr[j]])
+    })
+    return result
+}
+
+module.exports = arrayAnagram
---
+++ js-tasks/arrays/array-anagram.spec.js
@@ -0,0 +1,10 @@
+let assert = require("chai").assert;
+let arrayAnagram = require('./index.js')
+describe('Array to array of anagrams', function() {
+    it('should create array of anagrams', function() {
+        let arr = ['стол', 'барокко', 'слот', 'кот', 'кошка', 'ток', 'коробка']
+        let result = arrayAnagram(arr)
+        let expected = [['стол', 'слот'], ['барокко', 'коробка'], ['кот', 'ток']]
+        assert.deepEqual(result, expected)
+    })
+});
